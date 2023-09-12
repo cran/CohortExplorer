cc376555b2e771ced6a09f57f0c521011e2bd2c1
Gowtham Rao <rao@ohdsi.org>
2023-09-12 20:32:43 +0000

    version 0.0.17

---
+++ inst/doc/HowToUseCohortExplorer.R
@@ -0,0 +1,9 @@
+## ---- echo = FALSE, message = FALSE-------------------------------------------
+library(SqlRender)
+knitr::opts_chunk$set(
+  cache = FALSE,
+  comment = "#>",
+  error = FALSE,
+  tidy = FALSE
+)
+
